6829a6b4718c61eeaf2dfbf66d8dfb321320d2a7
admin <admin@YD03-22>
2021-06-02 16:31:47 +0900

    ..

---
+++ java_book/src/숙제/Rent.java
@@ -0,0 +1,13 @@
+package 숙제;
+
+public class Rent {
+	
+	private String name;
+	private String title;
+	private String date;
+	
+	public Rent() {
+	public Rent(String name, String title, String date)
+	}
+	
+}
---
+++ java_book/src/숙제/Service.java
@@ -0,0 +1,5 @@
+package 숙제;
+
+public interface Service {
+
+}
