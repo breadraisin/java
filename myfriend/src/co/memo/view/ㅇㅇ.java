29de7f1021bea0e75d3e2ecdc644ef2bc6f499d8
admin <admin@YD03-22>
2021-06-04 17:51:13 +0900

    dd

---
+++ myfriend/src/co/memo/view/ㅇㅇ.java
@@ -0,0 +1,19 @@
+package co.memo.view;
+
+import java.util.Scanner;
+
+public class ㅇㅇ {
+	public static void main(String[] args) {
+
+		int[] a;
+		Scanner s = new Scanner(System.in);
+		int b = s.nextInt();
+		a = new int[b];
+		for (int i = 0; i < b; i++) {
+			a[i] = s.nextInt();
+		}
+		
+		int max =
+				
+	}
+}
